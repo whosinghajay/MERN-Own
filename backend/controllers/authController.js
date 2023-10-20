ad13073d3112460fbfa088b5c8feafcb1503e1f5
Ajay Singh <whosinghajay@gmail.com>
2023-10-20 16:35:43 +0530

    backend signup created

---
+++ backend/controllers/authController.js
@@ -0,0 +1,22 @@
+import User from "../models/userModels.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { email, name, username, phoneNumber, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const user = new User({
+    email,
+    name,
+    username,
+    phoneNumber,
+    password: hashedPassword,
+  });
+  try {
+    await user.save();
+    res.status(201).json("User Created Successfully!!");
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
+
+export const login = async (req, res) => {};
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoutes.js";
+import authRouter from "./routes/authRoutes.js";
 
 dotenv.config();
 
@@ -16,8 +17,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
+app.use("/api/auth", authRouter)
 app.use("/api/user", userRouter);
